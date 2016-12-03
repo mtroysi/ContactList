b19f0e9fa50d0c72aec4c01153d0aaacbbddd03b
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2016-12-03 16:21:59 +0100

    Initial commit

---
+++ app/src/main/java/fr/m2sili/mtroysi/contactlist/ContactList.java
@@ -0,0 +1,13 @@
+package fr.m2sili.mtroysi.contactlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
