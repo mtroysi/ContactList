ec7c1b434b00a00d6d0e571ce105416d773320dc
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2016-12-04 14:15:58 +0100

    Ajout d'un contact

--- app/src/main/java/fr/m2sili/mtroysi/contactlist/ContactFragment.java
+++ app/src/main/java/fr/m2sili/mtroysi/contactlist/ContactFragment.java
@@ -1,13 +1,21 @@
 package fr.m2sili.mtroysi.contactlist;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by Morgane TROYSI on 12/3/16.
@@ -27,6 +35,7 @@ public class ContactFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_layout, container, false);
+        setHasOptionsMenu(true);
         adapter = new ContactAdapter(inflater.getContext(), R.layout.ligne);
         setListAdapter(adapter);
         adapter.add(new Contact("Dupont", "Jean", 26, 5, 1994, "test@test.fr"));
@@ -45,5 +54,44 @@ public class ContactFragment extends ListFragment {
         intent.putExtra("month", contact.getMonth());
         intent.putExtra("year", contact.getYear());
         startActivity(intent);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.options, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.new_contact:
+                LayoutInflater inflater = LayoutInflater.from(mainActivity);
+                final View alertDialogView = inflater.inflate(R.layout.alert_dialog,
+                        null);
+                AlertDialog.Builder adb = new AlertDialog.Builder(mainActivity);
+                adb.setView(alertDialogView);
+                adb.setTitle("Nouveau contact");
+                adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        EditText nom = (EditText) alertDialogView.findViewById(R.id.saisie_nom);
+                        EditText prenom = (EditText) alertDialogView.findViewById(R.id.saisie_prenom);
+                        EditText mail = (EditText) alertDialogView.findViewById(R.id.saisie_mail);
+                        DatePicker birthday = (DatePicker) alertDialogView.findViewById(R.id.saisie_ddn);
+                        Contact contact = new Contact(nom.getText().toString(), prenom.getText().toString(), birthday.getDayOfMonth(), birthday.getMonth() + 1, birthday.getYear(), mail.getText().toString());
+                        adapter.add(contact);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+
+                adb.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                adb.show();
+                break;
+        }
+        return true;
     }
 }
--- app/src/main/java/fr/m2sili/mtroysi/contactlist/DisplayFragment.java
+++ app/src/main/java/fr/m2sili/mtroysi/contactlist/DisplayFragment.java
@@ -30,6 +30,49 @@ public class DisplayFragment extends Fragment {
     public void displayContact(String name, String mail, int day, int month, int year) {
         this.name.setText(name);
         this.mail.setText(mail);
-        this.birthday.setText("Né le " + day + " " + month + " " + year);
+        this.birthday.setText("Né le " + day + " " + getMonthName(month) + " " + year);
+    }
+
+    public String getMonthName(int month) {
+        String monthName = "";
+        switch (month) {
+            case 1 :
+                monthName = "Janvier";
+                break;
+            case 2 :
+                monthName = "Février";
+                break;
+            case 3 :
+                monthName = "Mars";
+                break;
+            case 4 :
+                monthName = "Avril";
+                break;
+            case 5 :
+                monthName = "Mai";
+                break;
+            case 6 :
+                monthName = "Juin";
+                break;
+            case 7 :
+                monthName = "Juillet";
+                break;
+            case 8 :
+                monthName = "Aout";
+                break;
+            case 9 :
+                monthName = "Septembre";
+                break;
+            case 10 :
+                monthName = "Octobre";
+                break;
+            case 11 :
+                monthName = "Novembre";
+                break;
+            case 12 :
+                monthName = "Décembre";
+                break;
+        }
+        return monthName;
     }
 }
